9b6692e6c830ae552ea82955cc551bf390aacfe2
Simon Thulbourn <simon+github@thulbourn.com>
2013-10-20 22:33:55 +0100

    renames B.rb to b.rb

--- app/presenters/b.rb
+++ app/presenters/b.rb
@@ -1,13 +1,13 @@
-require 'app/presenters/Presenter'
-require 'app/models/B'
+require 'app/presenters/base'
+require 'app/models/b'
 
-class BPresenter < Presenter
+class B < Base
   attr_reader :run, :name, :age
 
   def initialize
     @run = true
 
-    load_model BModel.new('Mark', '99')
+    load_model B.new('Mark', '99')
     prepare_view_data({ :name => @model.name, :age => @model.age })
   end
 end
